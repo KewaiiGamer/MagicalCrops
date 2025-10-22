826c453932d4f5b48616e0fd50291362fd6a17c6
KewaiiGamer <miguelviegas1999@gmail.com>
2025-10-22 15:04:59 +0100

    feature: automate registration of recipes

--- src/main/java/magicalcrops/Main.java
+++ src/main/java/magicalcrops/Main.java
@@ -2,250 +2,44 @@ package magicalcrops;
 
 import magicalcrops.items.MagicalCrops;
 import necesse.engine.modLoader.annotations.ModEntry;
-import necesse.engine.registries.ItemRegistry;
 import necesse.engine.registries.RecipeTechRegistry;
 import necesse.inventory.item.Item;
-import necesse.inventory.recipe.Recipe;
-import necesse.inventory.recipe.Recipes;
-import necesse.level.gameObject.SeedObject;
 
-import java.awt.*;
-
-import static necesse.inventory.recipe.Recipes.ingredientsFromScript;
+import java.util.ArrayList;
 
 @ModEntry
 public class Main {
 
-  public void init() {
-    // Tier Essences
-    SeedObject.registerSeedObjects("kew_tier_1_seed", "kew_tier_1_crop", "kew_tier_1_essence", 1, 0, 5, 4200.0F, 6000.0F, new Color(90, 61, 48), 1.0F);
-    SeedObject.registerSeedObjects("kew_tier_2_seed", "kew_tier_2_crop", "kew_tier_2_essence", 1, 0, 5, 4200.0F, 6000.0F, new Color(90, 61, 48), 1.0F);
-    SeedObject.registerSeedObjects("kew_tier_3_seed", "kew_tier_3_crop", "kew_tier3_essence", 1, 0, 5, 4200.0F, 6000.0F, new Color(90, 61, 48), 1.0F);
-    SeedObject.registerSeedObjects("kew_tier_4_seed", "kew_tier_4_crop", "kew_tier42_essence", 1, 0, 5, 4200.0F, 6000.0F, new Color(90, 61, 48), 1.0F);
-    ItemRegistry.registerItem("kew_tier_1_essence", (new MagicalCrops(100, Item.Rarity.NORMAL).cropTexture("kew_tier_1_crop").addGlobalIngredient("anycompostable")), 2.0F, true);
-    ItemRegistry.registerItem("kew_tier_2_essence", (new MagicalCrops(100, Item.Rarity.COMMON).cropTexture("kew_tier_2_crop").addGlobalIngredient("anycompostable")), 2.0F, true);
-    ItemRegistry.registerItem("kew_tier_3_essence", (new MagicalCrops(100, Item.Rarity.UNCOMMON).cropTexture("kew_tier_3_crop").addGlobalIngredient("anycompostable")), 2.0F, true);
-    ItemRegistry.registerItem("kew_tier_4_essence", (new MagicalCrops(100, Item.Rarity.RARE).cropTexture("kew_tier_4_crop").addGlobalIngredient("anycompostable")), 2.0F, true);
-    // Tier 1 resources
-    SeedObject.registerSeedObjects("kew_fire_seed", "kew_fire_crop", "kew_fire_essence", 1, 0, 5, 4200.0F, 6000.0F, new Color(90, 61, 48), 1.0F);
-    SeedObject.registerSeedObjects("kew_water_seed", "kew_water_crop", "kew_water_essence", 1, 0, 5, 4200.0F, 6000.0F, new Color(90, 61, 48), 1.0F);
-    SeedObject.registerSeedObjects("kew_wood_seed", "kew_wood_crop", "kew_wood_essence", 1, 0, 5, 4200.0F, 6000.0F, new Color(90, 61, 48), 1.0F);
-    SeedObject.registerSeedObjects("kew_stone_seed", "kew_stone_crop", "kew_stone_essence", 1, 0, 5, 4200.0F, 6000.0F, new Color(90, 61, 48), 1.0F);
-    ItemRegistry.registerItem("kew_fire_essence", (new MagicalCrops(100, Item.Rarity.NORMAL).cropTexture("kew_fire_crop").addGlobalIngredient("anycompostable")), 2.0F, true);
-    ItemRegistry.registerItem("kew_water_essence", (new MagicalCrops(100, Item.Rarity.NORMAL).cropTexture("kew_water_crop").addGlobalIngredient("anycompostable")), 2.0F, true);
-    ItemRegistry.registerItem("kew_wood_essence", (new MagicalCrops(100, Item.Rarity.NORMAL).cropTexture("kew_wood_crop").addGlobalIngredient("anycompostable")), 2.0F, true);
-    ItemRegistry.registerItem("kew_stone_essence", (new MagicalCrops(100, Item.Rarity.NORMAL).cropTexture("kew_stone_crop").addGlobalIngredient("anycompostable")), 2.0F, true);
-    // Tier 2 Resources
-    SeedObject.registerSeedObjects("kew_copper_seed", "kew_copper_crop", "kew_copper_essence", 1, 0, 5, 4200.0F, 6000.0F, new Color(90, 61, 48), 1.0F);
-    SeedObject.registerSeedObjects("kew_iron_seed", "kew_iron_crop", "kew_iron_essence", 1, 0, 5, 4200.0F, 6000.0F, new Color(90, 61, 48), 1.0F);
-    SeedObject.registerSeedObjects("kew_gold_seed", "kew_gold_crop", "kew_gold_essence", 1, 0, 5, 4200.0F, 6000.0F, new Color(90, 61, 48), 1.0F);
-    ItemRegistry.registerItem("kew_copper_essence", (new MagicalCrops(100, Item.Rarity.COMMON).cropTexture("kew_copper_crop").addGlobalIngredient("anycompostable")), 2.0F, true);
-    ItemRegistry.registerItem("kew_iron_essence", (new MagicalCrops(100, Item.Rarity.COMMON).cropTexture("kew_iron_crop").addGlobalIngredient("anycompostable")), 2.0F, true);
-    ItemRegistry.registerItem("kew_gold_essence", (new MagicalCrops(100, Item.Rarity.COMMON).cropTexture("kew_gold_crop").addGlobalIngredient("anycompostable")), 2.0F, true);
-    // Tier 3 Resources
-    SeedObject.registerSeedObjects("kew_mycelium_seed", "kew_mycelium_crop", "kew_mycelium_essence", 1, 0, 5, 4200.0F, 6000.0F, new Color(90, 61, 48), 1.0F);
-    SeedObject.registerSeedObjects("kew_ancient_fossil_seed", "kew_ancient_fossil_crop", "kew_ancient_fossil_essence", 1, 0, 5, 4200.0F, 6000.0F, new Color(90, 61, 48), 1.0F);
-    SeedObject.registerSeedObjects("kew_glacial_seed", "kew_glacial_crop", "kew_glacial_essence", 1, 0, 5, 4200.0F, 6000.0F, new Color(90, 61, 48), 1.0F);
-    SeedObject.registerSeedObjects("kew_ivy_seed", "kew_ivy_crop", "kew_ivy_essence", 1, 0, 5, 4200.0F, 6000.0F, new Color(90, 61, 48), 1.0F);
-    ItemRegistry.registerItem("kew_mycelium_essence", (new MagicalCrops(100, Item.Rarity.UNCOMMON).cropTexture("kew_mycelium_crop").addGlobalIngredient("anycompostable")), 2.0F, true);
-    ItemRegistry.registerItem("kew_ancient_fossil_essence", (new MagicalCrops(100, Item.Rarity.UNCOMMON).cropTexture("kew_ancient_fossil_crop").addGlobalIngredient("anycompostable")), 2.0F, true);
-    ItemRegistry.registerItem("kew_glacial_essence", (new MagicalCrops(100, Item.Rarity.UNCOMMON).cropTexture("kew_glacial_crop").addGlobalIngredient("anycompostable")), 2.0F, true);
-    ItemRegistry.registerItem("kew_ivy_essence", (new MagicalCrops(100, Item.Rarity.UNCOMMON).cropTexture("kew_ivy_crop").addGlobalIngredient("anycompostable")), 2.0F, true);
-    // Tier 4 Resources
-    SeedObject.registerSeedObjects("kew_nightsteel_seed", "kew_nightsteel_crop", "kew_nightsteel_essence", 1, 0, 5, 4200.0F, 6000.0F, new Color(90, 61, 48), 1.0F);
-    ItemRegistry.registerItem("kew_nightsteel_essence", (new MagicalCrops(100, Item.Rarity.RARE).cropTexture("kew_nightsteel_crop").addGlobalIngredient("anycompostable")), 2.0F, true);
+  public ArrayList<MagicalCrops> magicalCrops = new ArrayList<>();
 
+  public void init() {
+    magicalCrops.add(new MagicalCrops(100, Item.Rarity.NORMAL, null, null, "tier_1", "essence", 1, RecipeTechRegistry.WORKSTATION));
+    magicalCrops.add(new MagicalCrops(100, Item.Rarity.COMMON, "copperbar,ironbar,goldbar", null, "tier_2", "essence", 2, RecipeTechRegistry.DEMONIC_WORKSTATION));
+    magicalCrops.add(new MagicalCrops(100, Item.Rarity.UNCOMMON, "demonicbar,ivybar", null, "tier_3", "essence", 3, RecipeTechRegistry.TUNGSTEN_WORKSTATION));
+    magicalCrops.add(new MagicalCrops(100, Item.Rarity.RARE, "ancientfossilbar,myceliumbar,glacialbar,tungstenbar", null, "tier_4", "essence", 4, RecipeTechRegistry.FALLEN_WORKSTATION));
+    magicalCrops.add(new MagicalCrops(100, Item.Rarity.NORMAL, "lavatile", null, "fire", "any", 1, RecipeTechRegistry.WORKSTATION));
+    magicalCrops.add(new MagicalCrops(100, Item.Rarity.NORMAL, "watertile", null, "water", "any", 1, RecipeTechRegistry.WORKSTATION));
+    magicalCrops.add(new MagicalCrops(100, Item.Rarity.NORMAL, "anylog", "oaklog,sprucelog,pinelog,palmlog,willowlog,birchlog,maplelog", "wood", "any", 1, RecipeTechRegistry.WORKSTATION));
+    magicalCrops.add(new MagicalCrops(100, Item.Rarity.NORMAL, "stone", "stone", "stone", "any", 1, RecipeTechRegistry.WORKSTATION));
+    magicalCrops.add(new MagicalCrops(100, Item.Rarity.COMMON, "copperbar", "copperore", "copper", "resource", 2, RecipeTechRegistry.WORKSTATION));
+    magicalCrops.add(new MagicalCrops(100, Item.Rarity.COMMON, "ironbar", "ironore", "iron", "resource", 2, RecipeTechRegistry.WORKSTATION));
+    magicalCrops.add(new MagicalCrops(100, Item.Rarity.COMMON, "goldbar", "goldore", "gold", "resource", 2, RecipeTechRegistry.WORKSTATION));
+    magicalCrops.add(new MagicalCrops(100, Item.Rarity.UNCOMMON, "ivybar", "ivyore", "ivy", "resource", 3, RecipeTechRegistry.TUNGSTEN_WORKSTATION));
+    //magicalCrops.add(new MagicalCrops(100, Item.Rarity.UNCOMMON, "demonicbar","demonicbar:16","demonic", "resource", 3, RecipeTechRegistry.TUNGSTEN_WORKSTATION));
+    magicalCrops.add(new MagicalCrops(100, Item.Rarity.RARE, "myceliumbar", "myceliumore:16", "mycelium", "resource", 4, RecipeTechRegistry.DEMONIC_WORKSTATION));
+    magicalCrops.add(new MagicalCrops(100, Item.Rarity.RARE, "ancientfossilbar", "ancientfossilore", "ancient_fossil", "resource", 4, RecipeTechRegistry.DEMONIC_WORKSTATION));
+    magicalCrops.add(new MagicalCrops(100, Item.Rarity.RARE, "glacialbar", "glacialore", "glacial", "resource", 4, RecipeTechRegistry.TUNGSTEN_WORKSTATION));
+    //magicalCrops.add(new MagicalCrops(100, Item.Rarity.RARE, "tungstenbar","tungstenore", "tungsten", "resource", 4, RecipeTechRegistry.FALLEN_WORKSTATION));
+    //magicalCrops.add(new MagicalCrops(100, Item.Rarity.EPIC, "nightsteelbar","nightsteelore","nightsteel", "resource", 5, RecipeTechRegistry.FALLEN_WORKSTATION));
+    for (MagicalCrops crop : magicalCrops) {
+      crop.doInit();
+    }
   }
 
   public void postInit() {
-    // Tier Seeds
-    Recipes.registerModRecipe(new Recipe(
-        "kew_tier_1_seed",
-        1,
-        RecipeTechRegistry.WORKSTATION,
-        ingredientsFromScript("{{kew_tier_1_essence, 8}, {grassseed, 1}}")
-    ));
-    Recipes.registerModRecipe(new Recipe(
-        "kew_tier_2_seed",
-        1,
-        RecipeTechRegistry.DEMONIC_WORKSTATION,
-        ingredientsFromScript("{{kew_tier_2_essence, 8}, {kew_tier_1_seed, 1}}")
-    ));
-    Recipes.registerModRecipe(new Recipe(
-        "kew_tier_3_seed",
-        1,
-        RecipeTechRegistry.TUNGSTEN_WORKSTATION,
-        ingredientsFromScript("{{kew_tier_3_essence, 8}, {kew_tier_2_seed, 1}}")
-    ));
-    Recipes.registerModRecipe(new Recipe(
-        "kew_tier_4_seed",
-        1,
-        RecipeTechRegistry.FALLEN_WORKSTATION,
-        ingredientsFromScript("{{kew_tier_4_essence, 8}, {kew_tier_3_seed, 1}}")
-    ));
-    // Tier Essences
-    Recipes.registerModRecipe(new Recipe(
-        "kew_tier_2_essence",
-        1,
-        RecipeTechRegistry.DEMONIC_WORKSTATION,
-        ingredientsFromScript("{{kew_tier_1_essence, 8}, {copperbar, 1}, {goldbar, 1}, {ironbar, 1}}")
-    ));
-    Recipes.registerModRecipe(new Recipe(
-        "kew_tier_3_essence",
-        1,
-        RecipeTechRegistry.TUNGSTEN_WORKSTATION,
-        ingredientsFromScript("{{kew_tier_2_essence, 8}, {demonicbar, 1, ivybar, 1}}")
-    ));
-    Recipes.registerModRecipe(new Recipe(
-        "kew_tier_4_essence",
-        1,
-        RecipeTechRegistry.FALLEN_WORKSTATION,
-        ingredientsFromScript("{{kew_tier_3_essence, 8}, {ancientfossilbar, 1}, {glacialbar, 1}, {tungstenbar, 1}, {myceliumbar, 1}}")
-    ));
-    // Tier 1 Seeds
-    Recipes.registerModRecipe(new Recipe(
-        "kew_water_seed",
-        1,
-        RecipeTechRegistry.WORKSTATION,
-        ingredientsFromScript("{{kew_tier_1_essence, 4}, {kew_tier_1_seed, 1}, {watertile, 4}}")
-    ));
-    Recipes.registerModRecipe(new Recipe(
-        "kew_fire_seed",
-        1,
-        RecipeTechRegistry.WORKSTATION,
-        ingredientsFromScript("{{kew_tier_1_essence, 4}, {kew_tier_1_seed, 1}, {lavatile, 4}}")
-    ));
-    Recipes.registerModRecipe(new Recipe(
-        "kew_wood_seed",
-        1,
-        RecipeTechRegistry.WORKSTATION,
-        ingredientsFromScript("{{kew_tier_1_essence, 4}, {kew_tier_1_seed, 1}, {anylog, 4}}")
-    ));
-    Recipes.registerModRecipe(new Recipe(
-        "kew_stone_seed",
-        1,
-        RecipeTechRegistry.WORKSTATION,
-        ingredientsFromScript("{{kew_tier_1_essence, 4}, {kew_tier_1_seed, 1}, {stone, 4}}")
-    ));
-    // Tier 2 Seeds
-    Recipes.registerModRecipe(new Recipe(
-        "kew_copper_seed",
-        1,
-        RecipeTechRegistry.DEMONIC_WORKSTATION,
-        ingredientsFromScript("{{kew_tier_2_essence, 4}, {kew_tier_2_seed, 1}, {copperbar, 4}}")
-    ));
-    Recipes.registerModRecipe(new Recipe(
-        "kew_iron_seed",
-        1,
-        RecipeTechRegistry.DEMONIC_WORKSTATION,
-        ingredientsFromScript("{{kew_tier_2_essence, 4}, {kew_tier_2_seed, 1}, {ironbar, 4}}")
-    ));
-    Recipes.registerModRecipe(new Recipe(
-        "kew_gold_seed",
-        1,
-        RecipeTechRegistry.DEMONIC_WORKSTATION,
-        ingredientsFromScript("{{kew_tier_2_essence, 4}, {kew_tier_2_seed, 1}, {goldbar, 4}}")
-    ));
-    // Tier 1 Resources
-    Recipes.registerModRecipe(new Recipe(
-        "stone",
-        1,
-        RecipeTechRegistry.WORKSTATION,
-        ingredientsFromScript("{{kew_stone_essence, 4}}")
-    ));
-    Recipes.registerModRecipe(new Recipe(
-        "oaklog",
-        1,
-        RecipeTechRegistry.WORKSTATION,
-        ingredientsFromScript("{{kew_wood_essence, 4}}")
-    ));
-    Recipes.registerModRecipe(new Recipe(
-        "sprucelog",
-        1,
-        RecipeTechRegistry.WORKSTATION,
-        ingredientsFromScript("{{kew_wood_essence, 4}}")
-    ));
-    Recipes.registerModRecipe(new Recipe(
-        "pinelog",
-        1,
-        RecipeTechRegistry.WORKSTATION,
-        ingredientsFromScript("{{kew_wood_essence, 4}}")
-    ));
-    Recipes.registerModRecipe(new Recipe(
-        "palmlog",
-        1,
-        RecipeTechRegistry.WORKSTATION,
-        ingredientsFromScript("{{kew_wood_essence, 4}}")
-    ));
-    Recipes.registerModRecipe(new Recipe(
-        "willowlog",
-        1,
-        RecipeTechRegistry.WORKSTATION,
-        ingredientsFromScript("{{kew_wood_essence, 4}}")
-    ));
-    Recipes.registerModRecipe(new Recipe(
-        "maplelog",
-        1,
-        RecipeTechRegistry.WORKSTATION,
-        ingredientsFromScript("{{kew_wood_essence, 4}}")
-    ));
-    Recipes.registerModRecipe(new Recipe(
-        "birchlog",
-        1,
-        RecipeTechRegistry.WORKSTATION,
-        ingredientsFromScript("{{kew_wood_essence, 4}}")
-    ));
-    // Tier 2 Resources
-    Recipes.registerModRecipe(new Recipe(
-        "copperore",
-        1,
-        RecipeTechRegistry.FORGE,
-        ingredientsFromScript("{{kew_copper_essence, 4}}")
-    ));
-    Recipes.registerModRecipe(new Recipe(
-        "ironore",
-        1,
-        RecipeTechRegistry.FORGE,
-        ingredientsFromScript("{{kew_iron_essence, 4}}")
-    ));
-    Recipes.registerModRecipe(new Recipe(
-        "goldore",
-        1,
-        RecipeTechRegistry.FORGE,
-        ingredientsFromScript("{{kew_gold_essence, 4}}")
-    ));
-    // Tier 3 Resources
-    Recipes.registerModRecipe(new Recipe(
-        "ivyore",
-        1,
-        RecipeTechRegistry.FORGE,
-        ingredientsFromScript("{{kew_ivy_essence, 4}}")
-    ));
-    Recipes.registerModRecipe(new Recipe(
-        "glacialore",
-        1,
-        RecipeTechRegistry.FORGE,
-        ingredientsFromScript("{{kew_glacial_essence, 4}}")
-    ));
-    Recipes.registerModRecipe(new Recipe(
-        "ancientfossilore",
-        1,
-        RecipeTechRegistry.FORGE,
-        ingredientsFromScript("{{kew_ancient_fossil_essence, 4}}")
-    ));
-    Recipes.registerModRecipe(new Recipe(
-        "myceliumore",
-        1,
-        RecipeTechRegistry.FORGE,
-        ingredientsFromScript("{{kew_mycelium_essence, 4}}")
-    ));
-    // Tier 4 Resources
-    Recipes.registerModRecipe(new Recipe(
-        "nightsteelore",
-        1,
-        RecipeTechRegistry.FORGE,
-        ingredientsFromScript("{{kew_nightsteel_essence, 4}}")
-    ));
+    for (MagicalCrops crop : magicalCrops) {
+      crop.doPostInit();
+    }
   }
 
 }
--- src/main/java/magicalcrops/items/MagicalCrops.java
+++ src/main/java/magicalcrops/items/MagicalCrops.java
@@ -1,27 +1,151 @@
 package magicalcrops.items;
 
+import necesse.engine.registries.ItemRegistry;
+import necesse.engine.registries.RecipeTechRegistry;
 import necesse.gfx.gameTexture.GameTexture;
 import necesse.inventory.item.Item;
 import necesse.inventory.item.matItem.MatItem;
+import necesse.inventory.recipe.Recipe;
+import necesse.inventory.recipe.Recipes;
+import necesse.inventory.recipe.Tech;
+import necesse.level.gameObject.SeedObject;
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static necesse.inventory.recipe.Recipes.ingredientsFromScript;
 
 public class MagicalCrops extends MatItem {
-    protected String cropTextureName;
+  protected String cropTextureName;
+  public String seedType;
+  public String resources;
+  public String crop;
+  public int tier;
+  public Tech tech;
+  public String outputResources;
 
-    public MagicalCrops(int stackSize, Item.Rarity rarity) {
-        super(stackSize, rarity);
-    }
+  public MagicalCrops(int stackSize, Item.Rarity rarity, String resources, String outputResources, String crop, String seedType, int tier, Tech tech) {
+    super(stackSize, rarity);
+    this.seedType = seedType;
+    this.crop = crop;
+    this.outputResources = outputResources;
+    this.resources = resources;
+    this.tier = tier;
+    this.tech = tech;
+  }
 
-    @Override
-    protected void loadItemTextures() {
-        if (this.cropTextureName != null) {
-            this.itemTexture = new GameTexture(GameTexture.fromFile("objects/" + this.cropTextureName), 0, 0, 32);
-        } else {
-            super.loadItemTextures();
-        }
+  @Override
+  protected void loadItemTextures() {
+    if (this.cropTextureName != null) {
+      this.itemTexture = new GameTexture(GameTexture.fromFile("objects/" + this.cropTextureName), 0, 0, 32);
+    } else {
+      super.loadItemTextures();
     }
+  }
 
-    public MagicalCrops cropTexture(String textureName) {
-        this.cropTextureName = textureName;
-        return this;
+  public MagicalCrops cropTexture(String textureName) {
+    this.cropTextureName = textureName;
+    return this;
+  }
+
+  public void doInit() {
+    registerCrop();
+    registerItem();
+  }
+
+  public void doPostInit() {
+    registerModRecipes();
+  }
+
+
+  public void registerCrop() {
+    SeedObject.registerSeedObjects(getSeedLocale(), getCropLocale(), getEssenceLocale(), 1, 0, 5, 4200.0F, 6000.0F, new Color(90, 61, 48), 1.0F);
+  }
+
+  public void registerItem() {
+
+    ItemRegistry.registerItem(getEssenceLocale(), this.cropTexture(getCropLocale()).addGlobalIngredient("anycompostable"), 1.0F, true);
+  }
+
+  public void registerModRecipes() {
+    registerEssenceRecipe();
+    registerSeedRecipe();
+  }
+
+  public void registerEssenceRecipe() {
+    if (resources == null) return;
+    if (seedType.equals("resource") || seedType.equals("any")) {
+
+      if (outputResources == null) return;
+      for (String outputResource : outputResources.split(",")) {
+        String[] parts = outputResource.split(":");
+        Recipes.registerModRecipe(new Recipe(
+            parts[0],
+            1,
+            seedType.equals("any") ? RecipeTechRegistry.WORKSTATION : RecipeTechRegistry.FORGE,
+            ingredientsFromScript(String.format("{{%s, %s}}", getEssenceLocale(), parts.length > 1 ? parts[1] : 4))
+        ));
+      }
+    } else if (seedType.equals("essence")) {
+      String formatted = Arrays.stream(resources.split(",")).map(s -> String.format("{%s, 1}", s))
+          .collect(Collectors.joining(", "));
+      Recipes.registerModRecipe(new Recipe(
+          getEssenceLocale(),
+          1,
+          tech,
+          ingredientsFromScript(String.format("{{%s, 8}, %s}", getPreviousTierSeedLocale(), formatted))
+      ));
     }
+  }
+
+  public void registerSeedRecipe() {
+    if (seedType.equals("resource") || seedType.equals("any")) {
+      if (resources == null) return;
+      String formatted = Arrays.stream(resources.split(","))
+          .map(s -> String.format("{%s, 4}", s))
+          .collect(Collectors.joining(", "));
+      Recipes.registerModRecipe(new Recipe(
+          getSeedLocale(),
+          1,
+          RecipeTechRegistry.TUNGSTEN_WORKSTATION,
+          ingredientsFromScript(String.format("{{%s, 4}, {%s, 1}, %s}", getTierEssenceLocale(), getTierSeedLocale(), formatted))
+      ));
+    } else if (seedType.equals("essence")) {
+      Recipes.registerModRecipe(new Recipe(
+          getSeedLocale(),
+          1,
+          RecipeTechRegistry.TUNGSTEN_WORKSTATION,
+          ingredientsFromScript(String.format("{{%s, 8}, {%s, 1}}", getEssenceLocale(), getPreviousTierSeedLocale()))
+      ));
+    }
+  }
+
+
+  public String getEssenceLocale() {
+    return String.format("kew_%s_essence", this.crop);
+  }
+
+  public String getSeedLocale() {
+    return String.format("kew_%s_seed", this.crop);
+  }
+
+  public String getCropLocale() {
+    return String.format("kew_%s_crop", this.crop);
+  }
+
+  public String getTierSeedLocale() {
+    return String.format("kew_tier_%s_seed", this.tier);
+  }
+
+  public String getTierEssenceLocale() {
+    return String.format("kew_tier_%s_essence", this.tier);
+  }
+
+  public String getPreviousTierSeedLocale() {
+    if (this.tier == 1) {
+      return "grassseed";
+    }
+    return String.format("kew_tier_%s_seed", this.tier - 1);
+  }
 }
